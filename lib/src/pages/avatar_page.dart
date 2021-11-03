4cf8f86dfd5d91a77d6ea1fcfda130b042561bc5
Franklin Failoc Ventura <franklin.failoc@tecsup.edu.pe>
2021-11-02 23:11:21 -0500

    alert commit

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,7 +6,7 @@ class AvatarPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Alerta'),
+        title: Text('Avatar'),
       ),
       body: CupertinoAlertDialog(
         title: const Text('Alert'),
